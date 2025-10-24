947bc8b82371caa12b8fce1d068a95f6c894a9d9
meb25072-coder <meb25072@tezu.ac.in>
2025-10-25 00:13:42 +0530

    Update intro.c

--- Lab_1/intro.c
+++ Lab_1/intro.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 int main(void) {
-  printf("Name: Sibam Kumar Shah\n");
-printf("Roll Number: MEB25020\n");
+  printf("Name: Kiron Kumar Dutta\n");
+printf("Roll Number: MEB25072\n");
 printf("Branch: Mechanical Engineering\n");
 printf("University: Tezpur University\n");
 }
